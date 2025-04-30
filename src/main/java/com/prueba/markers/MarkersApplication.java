48f6488add392d3a0a01e0c7c1f38e074df5b5b5
Jimmy Armando Nivia Barrera <jnivia@shd.gov.co>
2025-04-30 14:24:37 -0500

    new commit

---
+++ src/main/java/com/prueba/markers/MarkersApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.markers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkersApplication.class, args);
+	}
+
+}
